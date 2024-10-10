7b0ff9a7ae3325b2fbcdcce096e5e30d35d2fbb6
Urgen Sherpa <urgentimeero@gmail.com>
2024-10-10 19:57:13 +1100

    feat: add auto skipping functionality on question if more thatn 30 seconds elapsed

--- modules/games/routes.py
+++ modules/games/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, session, request
+from flask import render_template, redirect, url_for, session, request, flash
 from flask_login import login_required, current_user
 from .queries import get_games, get_questions_by_game_id
 
@@ -31,14 +31,21 @@ def question(id):
     current_question = questions[current_question_index]
     
     if request.method == 'POST':
-        user_answer = request.form.get('answer')
-        
-        if user_answer and int(user_answer) == current_question['correct_answer']:
-            session['score'] += 1
+     
         
         if request.form.get('action') == 'Skip':
             session['current_question'] += 1
             return redirect(url_for('games.question', id=id))
+        
+        user_answer = request.form.get('answer')
+        
+        if not user_answer:
+            # Flash an error message if no answer is selected
+            flash('Please select an option before submitting!', 'error')
+            return render_template('question.html', user=current_user, question=current_question, question_no=current_question_index + 1)
+        
+        if user_answer and int(user_answer) == current_question['correct_answer']:
+            session['score'] += 1
 
         # Move to next question
         session['current_question'] += 1
