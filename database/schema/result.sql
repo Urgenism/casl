90bcf1c09bfae8aa57fd75c8490a7d13afcfc5dd
Urgen Sherpa <urgentimeero@gmail.com>
2024-10-11 08:59:37 +1100

    feat: add style for results on question page

--- database/schema/result.sql
+++ database/schema/result.sql
@@ -1,9 +1,10 @@
 CREATE TABLE result (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    id_user INT NOT NULL, -- foreign key referencing the users table
-    id_game INT NOT NULL, -- foreign key referencing the games table
-    score INT NOT NULL, -- the score the user achieved in the game
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- timestamp of when the result was created
-    CONSTRAINT fk_user_result FOREIGN KEY (id_user) REFERENCES users(id), -- foreign key constraint for users
-    CONSTRAINT fk_game_result FOREIGN KEY (id_game) REFERENCES games(id)  -- foreign key constraint for games
+    id_user INT NOT NULL, 
+    id_game INT NOT NULL, 
+    score INT NOT NULL,
+    feedback TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user_result FOREIGN KEY (id_user) REFERENCES users(id),
+    CONSTRAINT fk_game_result FOREIGN KEY (id_game) REFERENCES games(id) 
 );
